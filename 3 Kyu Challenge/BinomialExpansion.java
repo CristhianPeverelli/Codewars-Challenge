4789185d52c419491d64b0c3348a438feb36b57f
Cristhian Peverelli <crispeve02@gmail.com>
2022-11-09 21:08:49 +0100

    Update BinomialExpansion.java

--- 3 Kyu Challenge/BinomialExpansion.java
+++ 3 Kyu Challenge/BinomialExpansion.java
@@ -1,10 +1,9 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class BinomialExpansion {
 
     public static void main(String[] args) {
-        System.out.println(expand("(1x-1)^2"));
+        expand("(5m+3)^4");
     }
 
     public static String expand(String expr) {
@@ -12,36 +11,41 @@ public class BinomialExpansion {
         int e = Integer.parseInt(input[1].substring(1));
         if(e == 0){ return "1"; }
         input[0] = input[0].substring(1);
+        System.out.println("Contenuto parentesi: "+input[0]);
+        System.out.println("Esponente: "+e);
 
-        //ax + b
-
-        String[] x = input[0].split("[a-zA-Z]");
-        int[] coeff = new int[x.length];
-        for(int i = 0; i < x.length; i++){
-            coeff[i] = Integer.parseInt(x[i]);
+        char lettera = 'a';
+        long first = 0,second = 0;
+        for(int i = 0; i < input[0].length(); i++){
+            if(isLiteral(input[0].charAt(i))){
+                lettera = input[0].charAt(i);
+                first = Long.parseLong(input[0].substring(0,i));
+                second = Long.parseLong(input[0].substring(i+1,input[0].length()));
+                break;
+            }
         }
 
-        String result = "";
+        long temp = 1;
+        long temp2 = 1;
+        long temp3 = 1;
         for(int i = 0; i < e; i++){
-            if(i != 0) {result += "+";}
-            long temp = getBinomialCoeff(e, i) * coeff[i];
-            if(temp != 1){ result += temp; }
-            if(e-i > 0){
-                result += "x";
-                if(e-i > 1){
-                    result += "^"+(e-i);
-                }
-            }
-            if(i>0){
-                result += "y";
-                if(i > 1){
-                    result += "^"+i;
-                }
-            }
-        }   
+            temp3 *= second;
+            temp *= getBinomialCoeff(e, i);
+        }
+        temp = temp * temp2 * temp3;
+        System.out.println(first+" "+second);
+        System.out.println(temp);
+        
 
-		return result;
-	}
+        return "";
+    }   
+
+    private static boolean isLiteral(char c){
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+            return true;
+        }
+        return false;
+    }
 
     private static long getBinomialCoeff(int n, int k){
         if(k > n){ return 0; }
@@ -55,21 +59,5 @@ public class BinomialExpansion {
         if(a.intValue() == 0){ return new BigInteger("1"); }
         return a.multiply(factorial(a.subtract(new BigInteger("1"))));
     }
+    
 }
-
-
-
-/* 
-        ArrayList<String> expressions = new ArrayList<String>();
-        int startIndex = 0;
-        for(int i = 0; i < input[0].length(); i++){
-            if(input[0].charAt(i) == '+' || input[0].charAt(i) == '-'){
-                if(startIndex == 0){ 
-                    expressions.add("+"+input[0].substring(startIndex,i));
-                }else{
-                    expressions.add(input[0].substring(startIndex,i));
-                }
-                startIndex = i;
-            }
-        }
-        expressions.add(input[0].substring(startIndex,input[0].length()));  */
